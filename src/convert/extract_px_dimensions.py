a1c2977e7ae8a4493cb4049490d175501c32617b
Lukas Martinelli <me@lukasmartinelli.ch>
2015-12-21 17:57:35 +0100

    Improve dimensions script

--- src/convert/extract_px_dimensions.py
+++ src/convert/extract_px_dimensions.py
@@ -13,34 +13,32 @@ import sys
 from docopt import docopt
 
 
-def extract_dimensions(reader):
+def extract_values(reader):
 
-    def parse_row(community, sex, nationality, row):
+    def parse_row(community, nationality, row):
         parts = community.split(' ')
         community_id = parts[0]
         community_name = " ".join(parts[1:])
-        return [community_id, community_name, sex, nationality] + row[4:]
+        return [community_id, community_name, nationality] + row[4:]
 
-    header_row = next(reader)
-    yield ['gemeinde_id', 'gemeinde_name', 'geschlecht', 'nationalität'] + header_row[4:]
+    def skip():
+        next(reader)
 
+    def parse_header_row():
+        header_row = next(reader)
+        additional_fields = ['community_id', 'community_name', 'nationality']
+        return additional_fields + header_row[4:]
+
+    yield parse_header_row()
     for row in reader:
         is_community = len(row) > 1 and row[1].startswith('......')
         if is_community:
             community = row[1].replace('......', '')
-            _ = next(reader)
 
-            swiss_male_row = next(reader)
-            yield parse_row(community, 'Mann', 'Schweiz', swiss_male_row)
-            swiss_female_row = next(reader)
-            yield parse_row(community, 'Frau', 'Schweiz', swiss_female_row)
-
-            _ = next(reader)
-
-            foreign_male_row = next(reader)
-            yield parse_row(community, 'Mann', 'Ausland', foreign_male_row)
-            foreign_female_row = next(reader)
-            yield parse_row(community, 'Frau', 'Ausland', foreign_female_row)
+            skip()
+            yield parse_row(community, 'Switzerland', next(reader))
+            skip()
+            yield parse_row(community, 'Foreign', next(reader))
 
 
 if __name__ == '__main__':
@@ -50,5 +48,5 @@ if __name__ == '__main__':
         reader = csv.reader(csv_file, delimiter='\t', quotechar='"')
         writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"')
 
-        for dim_row in extract_dimensions(reader):
-            writer.writerow(dim_row)
+        for row in extract_values(reader):
+            writer.writerow(row)
